d8c7404222270ba037104f6b831391cb62caa154
etmjr03 <etmjr03@gmail.com>
2022-10-13 16:02:48 -0300

    Importando tokens padroes do figma para o projeto

--- ignite-ds/src/App.tsx
+++ ignite-ds/src/App.tsx
@@ -4,9 +4,9 @@ export function App() {
 
   return (
     <>
-    <h1 className="font-bold text-5xl text-violet-500 text-center">Hello World</h1>
+    <h1 className="font-bold text-2xl text-violet-500 text-center">Hello World</h1>
     
-    <button className="bg-violet-500 font-medium px-4 py-2 rounded text-white hover:bg-violet-600">
+    <button className="bg-cyan-500 font-medium px-4 py-2 rounded text-white hover:bg-cyan-300">
       Enviar
     </button>
     </>
